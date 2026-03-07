fc674f07512fa1e7f360fca8cb50321ef14b772d
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-03-07 16:37:49 +0530

    Update mongo.py

--- Clonify/core/mongo.py
+++ Clonify/core/mongo.py
@@ -1,32 +1,27 @@
-from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
+from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo import MongoClient
-from pyrogram import Client
 
 import config
-
 from ..logging import LOGGER
 
-TEMP_MONGODB = ""
+# -----------------------------------------------------
 
+if not config.MONGO_DB_URI:
+    LOGGER(__name__).error("No MONGO_DB_URI found in environment!")
+    raise SystemExit("MongoDB URI is missing")
 
-if config.MONGO_DB_URI is None:
-    LOGGER(__name__).warning("No MONGO DB URL found. LOL")
-    temp_client = Client(
-        "Anon",
-        bot_token=config.BOT_TOKEN,
-        api_id=config.API_ID,
-        api_hash=config.API_HASH,
-    )
-    temp_client.start()
-    info = temp_client.get_me()
-    username = info.username
-    temp_client.stop()
-    _mongo_async_ = _mongo_client_(TEMP_MONGODB)
-    _mongo_sync_ = MongoClient(TEMP_MONGODB)
-    mongodb = _mongo_async_[username]
-    pymongodb = _mongo_sync_[username]
-else:
-    _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
+# -----------------------------------------------------
+
+try:
+    _mongo_async_ = AsyncIOMotorClient(config.MONGO_DB_URI)
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
+
+    # Database name
     mongodb = _mongo_async_.Anon
     pymongodb = _mongo_sync_.Anon
+
+    LOGGER(__name__).info("MongoDB Connected Successfully")
+
+except Exception as e:
+    LOGGER(__name__).error(f"MongoDB Connection Failed: {e}")
+    raise SystemExit
