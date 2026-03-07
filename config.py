2970bfe30be41fa54670f5cd1b7bda4d5f2c9ce9
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-03-07 15:13:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_ID = getenv("BOT_ID")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","purvibots")
+OWNER_USERNAME = getenv("OWNER_USERNAME","Oyekanhaa")
 # --------------------------------------------------------
 BOT_USERNAME = getenv("BOT_USERNAME" , "MayaMusicRobot")
 # --------------------------------------------------------
