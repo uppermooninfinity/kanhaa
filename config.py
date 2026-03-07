43683428a68698a57c4008bda850d95e110e9932
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-03-07 16:32:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 # ------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_ID = getenv("BOT_ID")
+BOT_ID = int(getenv("BOT_ID", "0"))
 # -------------------------------------------------------
 OWNER_USERNAME = getenv("OWNER_USERNAME","Oyekanhaa")
 # --------------------------------------------------------
